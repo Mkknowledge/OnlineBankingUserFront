43fd995f51521713f54d99156f3d4bb0389c39d0
Mkknowledge <kmayur2124@gmail.com>
2021-02-13 22:20:03 +0530

    added methods in userServiceImpl

--- src/main/java/com/userfront/service/userserviceimpl/UserServiceImpl.java
+++ src/main/java/com/userfront/service/userserviceimpl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.userfront.dao.UserDao;
+import com.userfront.domain.User;
 import com.userfront.domain.UserService;
 
 @Service
@@ -12,5 +13,39 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserDao userDao;
 	
+	public void save(User user) {
+		userDao.save(user);
+	}
+	
+	public User findByUsername(String username) {
+		return userDao.findByUsername(username);
+	}
+	
+	public User findByEmail(String email) {
+		return userDao.findByEmail(email);
+	}
+	
+	/*
+	 * public boolean checkUserExists(String username, String email) {
+	 * if(checkUsernameExists(username) || checkEmailExists(email)) {
+	 * 
+	 * } }
+	 */
+	
+	public boolean checkUsernameExists(String username) {
+		if(null != findByUsername(username)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean checkEmailExists(String email) {
+		if(null!= findByEmail(email)) {
+			return true;
+		}
+		
+		return false;
+	}
 	
 }
