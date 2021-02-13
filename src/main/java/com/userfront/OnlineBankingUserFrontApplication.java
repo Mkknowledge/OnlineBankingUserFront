50cba42a9b517d06954d3504e8999c7b3b56fbf2
Mkknowledge <kmayur2124@gmail.com>
2021-02-13 13:16:13 +0530

    initial commit

---
+++ src/main/java/com/userfront/OnlineBankingUserFrontApplication.java
@@ -0,0 +1,13 @@
+package com.userfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingUserFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingUserFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userfront/OnlineBankingUserFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingUserFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
