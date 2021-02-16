fc3b0a38d9d83e53d787d3458917fe91017f55ed
Mkknowledge <kmayur2124@gmail.com>
2021-02-16 21:29:26 +0530

    Savings Account and Primary Account DAO

---
+++ src/main/java/com/userfront/userservice/AccountService.java
@@ -0,0 +1,5 @@
+package com.userfront.userservice;
+
+public interface AccountService {
+
+}
--- src/main/java/com/userfront/userservice/userserviceimpl/UserSecurityService.java
+++ src/main/java/com/userfront/userservice/userserviceimpl/UserSecurityService.java
@@ -1,6 +1,5 @@
 package com.userfront.userservice.userserviceimpl;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +14,19 @@ import com.userfront.domain.User;
 @Service
 public class UserSecurityService implements UserDetailsService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
-	
-	@Autowired
-	private UserDao userDao;
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userDao.findByUsername(username);
-		if(null == user) {
-			LOG.warn("Username {} not found", username);
-			throw new UsernameNotFoundException("Username " + username + " not found");
-		}
-		return user;
-	}
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+        if (null == user) {
+            LOG.warn("Username {} not found", username);
+            throw new UsernameNotFoundException("Username " + username + " not found");
+        }
+        return user;
+    }
 }
