8c990efb299ef507d52dcc4a8192d2bfc67888c6
Mkknowledge <kmayur2124@gmail.com>
2021-02-18 00:09:36 +0530

    deposit activity completed

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -5,11 +5,14 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.User;
+import com.userfront.userservice.AccountService;
 import com.userfront.userservice.UserService;
 
 @Controller
@@ -18,6 +21,9 @@ public class AccountController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private AccountService accountService;
 
 	@RequestMapping("/primaryAccount")
 	public String primaryAccount(Principal principal, Model model) {
@@ -40,5 +46,35 @@ public class AccountController {
 		
 		return "savingsAccount";
 	}
+	
+	@RequestMapping(value = "/deposit", method = RequestMethod.GET)
+    public String deposit(Model model) {
+        model.addAttribute("accountType", "");
+        model.addAttribute("amount", "");
+
+        return "deposit";
+    }
+
+    @RequestMapping(value = "/deposit", method = RequestMethod.POST)
+    public String depositPOST(@ModelAttribute("amount") String amount, @ModelAttribute("accountType") String accountType, Principal principal) {
+        accountService.deposit(accountType, Double.parseDouble(amount), principal);
+
+        return "redirect:/userFront";
+    }
+    
+    @RequestMapping(value = "/withdraw", method = RequestMethod.GET)
+    public String withdraw(Model model) {
+        model.addAttribute("accountType", "");
+        model.addAttribute("amount", "");
+
+        return "withdraw";
+    }
+
+    @RequestMapping(value = "/withdraw", method = RequestMethod.POST)
+    public String withdrawPOST(@ModelAttribute("amount") String amount, @ModelAttribute("accountType") String accountType, Principal principal) {
+        accountService.withdraw(accountType, Double.parseDouble(amount), principal);
+
+        return "redirect:/userFront";
+    }
 	
 }
